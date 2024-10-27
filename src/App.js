06d03e59e7a69385513c69e3a4335920d1e9fab6
nsmairat <nilasmairat@gmail.com>
2024-10-27 07:27:58 -0700

    added a <p>

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
-import logo from "./logo.svg";
+import React from "react";
 import "./App.css";
 
 function App() {
   return (
     <form>
       <p>Weather App</p>
-
       <input type="search" placeholder="Type a city..." />
       <input type="submit" value="Search" />
+      <p>
+        <a href="/">Open-source</a> code, by <a href="/">Nila Smairat</a>
+      </p>
     </form>
   );
 }
