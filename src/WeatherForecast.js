5c80b811c0a05e42c26ab6a20ac781c8d36ec328
nsmairat <nilasmairat@gmail.com>
2024-11-24 15:13:34 -0800

    used  useEffect and refined my code

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,23 +1,36 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./WeatherForecast.css";
 import WeatherForecastDay from "./WeatherForecastDay";
 import Axios from "axios";
 
 export default function WeatherForecast(props) {
-  const [loaded, setLoaded] = useState(false);
-  const [forecast, setForecast] = useState(null);
+  let [loaded, setLoaded] = useState(false);
+  let [forecast, setForecast] = useState(null);
+
+  useEffect(() => {
+    setLoaded(false);
+  }, [props.coordinates]);
+
   function handleResponse(response) {
     console.log(response.data);
-    setForecast(response.data);
+    setForecast(response.data.daily);
     setLoaded(true);
   }
   if (loaded) {
     return (
       <div className="WeatherForecast">
-        <div className="raw">
-          <div className="col">
-            <WeatherForecastDay data={forecast.daily[0]} />
-          </div>
+        <div className="row">
+          {forecast.map(function (dailyForecast, index) {
+            if (index < 5) {
+              return (
+                <div className="col" key={index}>
+                  <WeatherForecastDay data={dailyForecast} />
+                </div>
+              );
+            } else {
+              return null;
+            }
+          })}
         </div>
       </div>
     );
